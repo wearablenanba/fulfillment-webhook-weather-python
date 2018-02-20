c30c6ca9f6de06c8e353093f70ddaaa8173f4f37
wearablenanba <wearabe20180216@gmail.com>
2018-02-20 17:46:25 +0900

    yamamoto4

--- app.py
+++ app.py
@@ -27,7 +27,10 @@ import os
 
 from flask import Flask
 from flask import request
-from flask import make_response
+from flask import make_response, jsonify
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -36,82 +39,25 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-
-def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+    weapon_name = parameters.get("weapon_name")
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    scope = ['https://spreadsheets.google.com/feeds']
 
+    #ダウンロードしたjsonファイルを同じフォルダに格納して指定する
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('xxxxxxxxxx.json', scope)
+    gc = gspread.authorize(credentials)
 
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
+    # # 共有設定したスプレッドシートの名前を指定する
+    worksheet = gc.open("Google Assistant Commands").get_worksheet(1)
 
-    result = query.get('results')
-    if result is None:
-        return {}
+    cell = worksheet.find(weapon_name)
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
-             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-
+    text = str(cell.value) + str(worksheet.cell(cell.row,cell.col+1).value) + "パーセント"
+    r = make_response(jsonify({'speech':text,'displayText':text}))
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
